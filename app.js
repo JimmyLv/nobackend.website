bc99919bbe9a365895e6340e116bc71c9fa743a3
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-02-20 15:59:49 +0800

    [#3] - Add angular-cache for github api and clean code.

--- app.js
+++ app.js
@@ -3,9 +3,17 @@ angular
     'ngRoute',
     'base64',
     'ab-base64',
-    'hc.marked'
+    'hc.marked',
+    'angular-cache'
   ])
-  .config(config);
+  .config(config)
+  .run(function ($http, CacheFactory) {
+    $http.defaults.cache = CacheFactory('defaultCache', {
+      maxAge: 15 * 60 * 1000, // Items added to this cache expire after 15 minutes
+      cacheFlushInterval: 60 * 60 * 1000, // This cache will clear itself every hour
+      deleteOnExpire: 'aggressive' // Items will be deleted from this cache when they expire
+    });
+  });
 
 function config($routeProvider) {
   $routeProvider
@@ -28,10 +36,14 @@ function config($routeProvider) {
     template: '<posts post="$resolve.post.data" categories="$resolve.categories.data"></posts>',
     resolve: {
       post: function ($http) {
-        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts/%E7%BC%96%E7%A8%8B/2014-11-15-pythonic-zen.md?ref=gh-pages')
+        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts/%E7%BC%96%E7%A8%8B/2014-11-15-pythonic-zen.md?ref=gh-pages', {
+          cache: true
+        })
       },
       categories: function ($http) {
-        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts?ref=gh-pages')
+        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts?ref=gh-pages', {
+          cache: true
+        })
       }
     }
   });
--- app/components/jekyll/posts.js
+++ app/components/jekyll/posts.js
@@ -1,36 +1,18 @@
 angular.module('app')
   .component('posts', {
-    templateUrl: './app/components/jekyllPosts/posts.html',
+    templateUrl: './app/components/jekyll/posts.html',
     bindings: {
       post: '<',
       categories: '<'
     },
     controller: ['$http', 'base64', function ($http, base64) {
-      //var github = new Github({
-      //  token: "4b417dc371cf5803a9da4efba13bab5eacacc533",
-      //  auth: "oauth"
-      //});
-      //
-      //var repo = github.getRepo('JimmyLv', 'Jimmy.lv');
-      //console.info(repo.read('gh-pages', '_posts/%E7%BC%96%E7%A8%8B/2014-11-15-pythonic-zen.md', function(err, data) {}));
-      //
-
-
-      var categories = this.categories;
-      var postsWithCategories = [];
-
-      console.info('post:', this.post);
-      console.info('categories:', this.categories);
-
-      categories.forEach(function (category) {
+      this.categories.forEach(function (category) {
           $http.get(category.url).then(function (res) {
-            console.info(category.name);
             var postItems = res.data.splice(0, 1);
             category.postItems = postItems;
 
             postItems.forEach(function (postItem) {
                 $http.get(postItem.url).then(function (res) {
-                    console.info('postItem:', res.data);
                     var rawPost = res.data;
                     var decodedContent = base64.decode(rawPost.content);
                     rawPost.editUrl = rawPost.html_url.replace('blob', 'edit');
@@ -44,11 +26,5 @@ angular.module('app')
           })
         }
       );
-
-      console.info(categories);
-
-
-      this.posts = [this.post];
-
     }]
   });
