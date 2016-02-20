faa9345129f02cade12b8d0812e96887c97565bb
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-02-20 15:10:10 +0800

    [#2] - Release 0x01: Finish changes about zuimeia and jekyll blog.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 angular
-  .module('app', ['ngRoute'])
+  .module('app', [
+    'ngRoute',
+    'base64',
+    'ab-base64',
+    'hc.marked'
+  ])
   .config(config);
 
 function config($routeProvider) {
@@ -12,11 +17,22 @@ function config($routeProvider) {
         }
       }
     })
-    .when('/appList', {
+    .when('/zuimeia', {
       template: '<app-list app-items="$resolve.appItems"></app-list>',
       resolve: {
         appItems: function ($http) {
-          return $http.get('data.json')}
+          return $http.get('data.json')
+        }
       }
-    });
+    }).when('/jekyll', {
+    template: '<posts post="$resolve.post.data" categories="$resolve.categories.data"></posts>',
+    resolve: {
+      post: function ($http) {
+        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts/%E7%BC%96%E7%A8%8B/2014-11-15-pythonic-zen.md?ref=gh-pages')
+      },
+      categories: function ($http) {
+        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts?ref=gh-pages')
+      }
+    }
+  });
 }
