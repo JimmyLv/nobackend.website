e86dfc2dbe5f12e7515b1e47dc583f1b8beb7c4e
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-02-20 19:51:36 +0800

    [#4] - Add highlight for code block and try to render markdown inside controller.

--- app.js
+++ app.js
@@ -1,12 +1,60 @@
 angular
   .module('app', [
     'ngRoute',
-    'base64',
+    'ngSanitize',
     'ab-base64',
     'hc.marked',
     'angular-cache'
   ])
-  .config(config)
+  .config(['$routeProvider', 'markedProvider', function ($routeProvider, markedProvider) {
+    markedProvider.setOptions({
+      gfm: true,
+      tables: true,
+      highlight: function (code, lang) {
+        if (lang) {
+          return hljs.highlight(lang, code, true).value;
+        } else {
+          return hljs.highlightAuto(code).value;
+        }
+      },
+      link: function (href, title, text) {
+        return "<a href='" + href + "'" + (title ? " title='" + title + "'" : '') + " target='_blank'>" + text + "</a>";
+      }
+    });
+
+    $routeProvider
+      .when('/hello', {
+        template: '<hello name="$resolve.name"></hello>',
+        resolve: {
+          name: function () {
+            return 'jimmy';
+          }
+        }
+      })
+      .when('/zuimeia', {
+        template: '<app-list app-items="$resolve.appItems"></app-list>',
+        resolve: {
+          appItems: function ($http) {
+            return $http.get('data.json')
+          }
+        }
+      })
+      .when('/jekyll', {
+        template: '<posts post="$resolve.post.data" categories="$resolve.categories.data"></posts>',
+        resolve: {
+          post: function ($http) {
+            return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts/%E7%BC%96%E7%A8%8B/2014-11-15-pythonic-zen.md?ref=gh-pages', {
+              cache: true
+            })
+          },
+          categories: function ($http) {
+            return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts?ref=gh-pages', {
+              cache: true
+            })
+          }
+        }
+      });
+  }])
   .run(function ($http, CacheFactory) {
     $http.defaults.cache = CacheFactory('defaultCache', {
       maxAge: 15 * 60 * 1000, // Items added to this cache expire after 15 minutes
@@ -14,37 +62,3 @@ angular
       deleteOnExpire: 'aggressive' // Items will be deleted from this cache when they expire
     });
   });
-
-function config($routeProvider) {
-  $routeProvider
-    .when('/hello', {
-      template: '<hello name="$resolve.name"></hello>',
-      resolve: {
-        name: function () {
-          return 'jimmy';
-        }
-      }
-    })
-    .when('/zuimeia', {
-      template: '<app-list app-items="$resolve.appItems"></app-list>',
-      resolve: {
-        appItems: function ($http) {
-          return $http.get('data.json')
-        }
-      }
-    }).when('/jekyll', {
-    template: '<posts post="$resolve.post.data" categories="$resolve.categories.data"></posts>',
-    resolve: {
-      post: function ($http) {
-        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts/%E7%BC%96%E7%A8%8B/2014-11-15-pythonic-zen.md?ref=gh-pages', {
-          cache: true
-        })
-      },
-      categories: function ($http) {
-        return $http.get('https://api.github.com/repos/JimmyLv/Jimmy.lv/contents/_posts?ref=gh-pages', {
-          cache: true
-        })
-      }
-    }
-  });
-}
--- app/components/jekyll/posts.js
+++ app/components/jekyll/posts.js
@@ -5,7 +5,7 @@ angular.module('app')
       post: '<',
       categories: '<'
     },
-    controller: ['$http', 'base64', function ($http, base64) {
+    controller: ['$http', 'base64', 'marked', function ($http, base64, marked) {
       this.categories.forEach(function (category) {
           $http.get(category.url).then(function (res) {
             var postItems = res.data.splice(0, 1);
@@ -17,6 +17,7 @@ angular.module('app')
                     var decodedContent = base64.decode(rawPost.content);
                     rawPost.editUrl = rawPost.html_url.replace('blob', 'edit');
                     rawPost.content = decodedContent.split('---')[2];
+                    rawPost.myHTML = marked(rawPost.content);
                     rawPost.meta = jsyaml.load(decodedContent.split('---')[1]);
                     postItem.postContent = rawPost;
                   }
