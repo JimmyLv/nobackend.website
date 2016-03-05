93f23b30b593d5c5e75f55344ea778e4b8d4d9a7
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-03-05 22:32:08 +0800

    [#17] - Release 0x07: Add random post and search functionality.

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -1,9 +1,22 @@
 angular.module('app')
-  .controller('MainCtrl', function (githubService) {
+  .controller('MainCtrl', ['$location', 'githubService', function ($location, githubService) {
     var vm = this;
     vm.message = 'hello';
+    vm.clearSearch = function () {
+      delete vm.searchText;
+    };
+    vm.randomPost = function () {
+      var post = vm.posts[Math.floor(Math.random() * vm.posts.length)];
+      console.info('random post:', post);
+      $location.path('note/' + post.category + post.url)
+    };
 
     githubService.getConfig().then(function (res) {
       vm.config = res.data;
     });
-  });
+
+    githubService.getIndex().then(function (res) {
+      vm.posts = res.data.paginator;
+      console.info('initial data:', vm.posts);
+    });
+  }]);
