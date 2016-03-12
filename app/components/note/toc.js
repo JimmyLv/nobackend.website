0b180029658644bc018d2b79124604c2b4e846a6
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-03-12 12:16:07 +0800

    [#45] - Refactored toc functionality.

--- app/components/note/toc.js
+++ app/components/note/toc.js
@@ -3,46 +3,40 @@ angular.module('app')
     return {
       require: '?ngModel',
       scope: true,
-      bindToController: {
-        tocHtml: '='
-      },
       controller: function () {
-        console.info('$scope.tocHtml', this.tocHtml);
+        this.headlines = [];
+        this.scrollTo = function (headline) {
+          console.info('selectedHeadline', headline.element.id);
+
+          //headline.element.scrollIntoView();
+          $location.hash(headline.element.id);
+
+          $anchorScroll();
+        };
       },
       controllerAs: 'toc',
-      link: function (scope, elm, attrs) {
-        console.info('$scope:', scope);
-        console.info('$scope.toc:', scope.toc);
-        console.info('tocHtml:', scope.toc.tocHtml);
-        function updateHeadlines() {
-          scope.headlines = [];
-          angular.forEach(elm[0].querySelectorAll('h1,h2,h3,h4,h5,h6'), function (e) {
-            scope.headlines.push({
+      link: function ($scope, $element) {
+
+        $scope.toc.headlines = updateHeadlines($element[0]);
+
+        // avoid memoryleaks from dom references
+        $scope.$on('$destroy', function () {
+          $scope.toc.headlines = [];
+        });
+
+        function updateHeadlines(content) {
+          var headlines = [];
+          angular.forEach(content.querySelectorAll('h1,h2,h3,h4,h5,h6'), function (e) {
+            headlines.push({
               level: e.tagName[1],
               label: angular.element(e).text(),
               element: e
             });
           });
-          console.info('headlines', scope.headlines);
+          return headlines;
         }
 
-        // avoid memoryleaks from dom references
-        scope.$on('$destroy', function () {
-          scope.headlines = [];
-        });
-        // scroll to one of the headlines
-        scope.scrollTo = function (headline) {
-          console.info('selectedHeadline', headline.element.id);
-          //headline.element.scrollIntoView();
-
-          $location.hash(headline.element.id);
-
-          // call $anchorScroll()
-          $anchorScroll();
-        };
-        // when the html updates whe update the headlines
-        //ngModel.$render = updateHeadlines;
-        updateHeadlines();
+        console.info('$scope', $scope);
       }
     }
   });
