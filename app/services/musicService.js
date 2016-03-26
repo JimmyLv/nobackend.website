dc341defa3baa09b66a91793fdd39be318b044b3
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-03-27 00:46:53 +0800

    [#19] - Wrapped netease music api, totally three type.

--- app/services/musicService.js
+++ app/services/musicService.js
@@ -5,11 +5,27 @@ class MusicService {
     this.$http = $http;
   }
 
-  getPlayList(listId) {
-    return this.$http.jsonp(`http://app.atime.me/music-api-server/?p=netease&t=playlist&i=${listId}&c=JSON_CALLBACK`, {
+  getPlayList(id) {
+    return this.$http.jsonp(_url('playlist',id), {
       cache: true
     })
   }
+
+  getMusic(ids) {
+    return this.$http.jsonp(_url('songlist',ids), {
+      cache: true
+    })
+  }
+
+  getAlbum(id) {
+    return this.$http.jsonp(_url('album',id), {
+      cache: true
+    })
+  }
+}
+
+function _url(type, id) {
+  return `http://app.atime.me/music-api-server/?p=netease&t=${type}&i=${id}&c=JSON_CALLBACK`
 }
 
 export default MusicService;
