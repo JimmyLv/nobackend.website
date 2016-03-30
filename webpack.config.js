36c304d0f82474b0e7686d05837c0f7b8b5d1c62
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-03-30 22:58:58 +0800

    [#62] - Extract JS library into separate vendor.js and style text to app.css after autoprefixer.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,40 @@
 var path = require('path');
+var args = require('yargs').argv;
+
 var webpack = require('webpack');
+var precss       = require('precss');
+var autoprefixer = require('autoprefixer');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+// parameters
+var isProd = args.prod;
 
 module.exports = {
   context: __dirname,
   entry: {
-    app: ['webpack/hot/dev-server', './app/app.js']
+    app: ['webpack/hot/dev-server', './app/app.js'],
+    vendor: [
+      // angular
+      'angular',
+      'angular-ui-router',
+      'angular-route',
+      'angular-new-router',
+      'angular-sanitize',
+      'angular-animate',
+
+      // 3rd dependencies
+      'ng-fx',
+      'angular-cache',
+      'angular-marked',
+      'angularUtils-disqus',
+      'angular-loading-bar',
+      'angulartics',
+      'angular-socialshare',
+      'angulartics-google-analytics',
+      'js-yaml/lib/js-yaml.js',
+      'lowdb',
+      'lowdb/browser'
+    ]
   },
   output: {
     path: './dist',
@@ -14,17 +44,22 @@ module.exports = {
   module: {
     loaders: [
       {test: /\.js$/, exclude: /node_modules/, loader: 'ng-annotate?add=true!babel-loader'},
-      {test: /\.css$/, loader: "style!css"},
-      {test: /\.less$/, loader: "style!css!less"},
+      {test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss')},
+      {test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css?sourceMap!postcss!less')},
       {test: /\.(eot|woff|woff2|ttf|svg)(\?\S*)?$/, loader: 'url?limit=100000&name=./fonts/[name].[ext]'},
       {test: /\.(png|jpe?g|gif)$/, loader: 'url-loader?limit=8192&name=./images/[hash].[ext]'},
       {test: /\.html$/, loader: 'ngtemplate!html?attrs[]=img:src img:ng-src'}
     ],
     noParse: []
   },
+  postcss: function () {
+    return [precss, autoprefixer];
+  },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin()
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.optimize.CommonsChunkPlugin('vendor', 'vendor.js'),
+    new ExtractTextPlugin('[name].css')
   ],
 
   resolve: {
