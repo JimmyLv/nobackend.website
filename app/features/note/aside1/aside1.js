385ef7efdabd43675b1e4f80b81f29935dcc27a2
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-04-01 12:58:42 +0800

    [#56] - Fixed missing email href on the bottom of aside1.

--- app/features/note/aside1/aside1.js
+++ app/features/note/aside1/aside1.js
@@ -11,6 +11,7 @@ export default {
 
     const vm = this;
     vm.$onInit = () => {
+      vm.author = configService.config.author;
       vm.locals = configService.config.locals;
       vm.rssUrl = configService.config.posts.api.rss;
     }
