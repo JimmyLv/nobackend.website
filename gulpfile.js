a29e0e8297db44a3ff0087f2b50deb3114787490
Jimmy Lv <jimmy.jinglv@gmail.com>
2016-04-13 01:02:47 +0800

    [#6] - Remove some useless code and files.

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var webpack = require('webpack-stream');
-
-gulp.task('webpack', function () {
-  return gulp.src('./app.js')
-    .pipe(require('./webpack.config.js'))
-    .pipe(gulp.dest('assets/'));
-});
